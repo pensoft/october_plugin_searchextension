fceb5c9aea3992d98eb0014298def8f88e05e6a1
root <root@project-dev.pensoft.local>
2023-01-06 11:11:19 +0200

    пресс релеасес сеарцх фиь

--- providers/PressreleasesServiceProvider.php
+++ providers/PressreleasesServiceProvider.php
@@ -14,15 +14,15 @@ class PressreleasesServiceProvider extends ResultsProvider
             $matching = \Pensoft\Media\Models\Pressreleases::where('name', 'ilike', "%{$this->query}%")
                 ->orWhere('description', 'ilike', "%{$this->query}%")
                 ->get();
-    
+
             // Create a new Result for every match
             foreach ($matching as $match) {
                 $result            = $this->newResult();
-    
+
                 $result->relevance = 1;
                 $result->title     = $match->name;
                 $result->text      = $match->description ?: '';
-                $result->url       = $match->link ?: $controller->pageUrl('press-releases');
+//                $result->url       = $match->link ?: $controller->pageUrl('press-releases');
                 // $result->thumb     = null;
                 $result->model     = $match;
                 $result->meta      = [
@@ -31,8 +31,8 @@ class PressreleasesServiceProvider extends ResultsProvider
                 if(!$match->link){
                     unset($result->meta['target']);
                 }
-    
-    
+
+
                 // Add the results to the results collection
                 $this->addResult($result);
             }
