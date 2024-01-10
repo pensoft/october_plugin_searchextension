eab00afc3053cdca38aa6ad25a17308711d4f496
root <root@project-dev.pensoft.local>
2024-01-10 12:07:02 +0200

    fixed partners search

--- providers/PartnersServiceProvider.php
+++ providers/PartnersServiceProvider.php
@@ -11,20 +11,20 @@ class PartnersServiceProvider extends ResultsProvider
         $controller = \Cms\Classes\Controller::getController() ?? new \Cms\Classes\Controller();
         // Get your matching models
         if(class_exists(\Pensoft\Partners\Models\Partners::class)){
-            $matching = \Pensoft\Partners\Models\Partners::where('content', 'ilike', "%{$this->query}%")->get();
-    
+            $matching = \Pensoft\Partners\Models\Partners::where('instituion', 'ilike', "%{$this->query}%")
+                ->orWhere('content', 'ilike', "%{$this->query}%")->get();
+
             // Create a new Result for every match
             foreach ($matching as $match) {
                 $result            = $this->newResult();
-    
                 $result->relevance = 1;
                 $result->title     = '';
                 $result->text      = $match->content;
-                $result->url       = $controller->pageUrl('partners');
+                $result->url       = $controller->pageUrl('partners', ['code' => $match->id]);
                 // $result->thumb     = $match->cover;
                 $result->model     = $match;
                 $result->meta      = [];
-    
+
                 // Add the results to the results collection
                 $this->addResult($result);
             }
