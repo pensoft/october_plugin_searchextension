b5b934c8b23c108397700a6e373c03d89758f959
root <root@project-dev.pensoft.local>
2024-08-09 09:13:39 +0300

    search fix

--- providers/UsersServiceProvider.php
+++ providers/UsersServiceProvider.php
@@ -9,33 +9,37 @@ class UsersServiceProvider extends ResultsProvider
 {
     public function search()
     {
-        $user = Auth::getUser();
-        $controller = \Cms\Classes\Controller::getController() ?? new \Cms\Classes\Controller();
-        // Get your matching models
-        if(class_exists(\Rainlab\User\Models\User::class) && $user){
-            $matching = \Rainlab\User\Models\User::where('name', 'ilike', "%{$this->query}%")
-                ->orWhere('surname', 'ilike', "%{$this->query}%")
-                ->orWhere('email', 'ilike', "%{$this->query}%")
-                ->orWhereRaw('( name || \' \'|| surname ) ilike ?', "%{$this->query}%")
-                ->get();
+        if(class_exists(\Rainlab\User\Models\User::class)){
+            $user = Auth::getUser();
+            $controller = \Cms\Classes\Controller::getController() ?? new \Cms\Classes\Controller();
+            // Get your matching models
+            if($user){
+                $matching = \Rainlab\User\Models\User::where('name', 'ilike', "%{$this->query}%")
+                    ->orWhere('surname', 'ilike', "%{$this->query}%")
+                    ->orWhere('email', 'ilike', "%{$this->query}%")
+                    ->orWhereRaw('( name || \' \'|| surname ) ilike ?', "%{$this->query}%")
+                    ->get();
 
-            // Create a new Result for every match
-            foreach ($matching as $match) {
-                $result            = $this->newResult();
+                // Create a new Result for every match
+                foreach ($matching as $match) {
+                    $result            = $this->newResult();
 
-                $result->relevance = 1;
-                $result->title     = $match->name.' '.$match->surname;
-                $result->text      = $match->email;
-                $result->url       = $controller->pageUrl('users').'#'.$match->email;
-                // $result->thumb     = $match->cover_image;
-                $result->model     = $match;
-                $result->meta      = [
-                ];
+                    $result->relevance = 1;
+                    $result->title     = $match->name.' '.$match->surname;
+                    $result->text      = $match->email;
+                    $result->url       = $controller->pageUrl('users').'#'.$match->email;
+                    // $result->thumb     = $match->cover_image;
+                    $result->model     = $match;
+                    $result->meta      = [
+                    ];
 
-                // Add the results to the results collection
-                $this->addResult($result);
+                    // Add the results to the results collection
+                    $this->addResult($result);
+                }
             }
         }
+
+
 
         return $this;
     }
